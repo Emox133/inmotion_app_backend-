694c7f16a208adbc983bdad4913df68c05dfa2ec
Emox133 <emir.betaacc0unt@gmail.com>
2020-09-06 10:07:13 +0200

    cors4

--- app.js
+++ app.js
@@ -1,10 +1,7 @@
 const express = require('express')
 const cors = require('cors')
-<<<<<<< HEAD
-const morgan = require('morgan')
-=======
 // const morgan = require('morgan')
->>>>>>> 25421610e2622a0447bb2a9970c970fcf36693eb
+
 const path = require('path')
 const app = express()
 const globalErrorHandler = require('./controllers/errorController')
@@ -18,11 +15,7 @@ app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 
 // Development middleware
-<<<<<<< HEAD
-app.use(morgan('dev'))
-=======
 // app.use(morgan('dev'))
->>>>>>> 25421610e2622a0447bb2a9970c970fcf36693eb
 
 // Routers
 app.use('/api/v1/users', userRouter)
